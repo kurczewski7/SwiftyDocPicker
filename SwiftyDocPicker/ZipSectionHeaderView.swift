646ed2fed8fbaf4acdbb222de52f04a97821bab0
Sławomir Kurczewski <slawomir.kurczewski@gmail.com>
2020-03-19 23:53:16 +0100

    Added Collection View Sections header. OK

---
+++ SwiftyDocPicker/ZipSectionHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ZipSectionHeaderView.swift
+//  SwiftyDocPicker
+//
+//  Created by Slawek Kurczewski on 19/03/2020.
+//  Copyright © 2020 Abraham Mangona. All rights reserved.
+//
+
+import UIKit
+
+class ZipSectionHeaderView: UICollectionReusableView {
+    @IBOutlet weak var label: UILabel!
+}
--- SwiftyDocPicker/ZipViewController.swift
+++ SwiftyDocPicker/ZipViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ZipViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "zipCell", for: indexPath) as! ZipCollectionViewCell
@@ -19,14 +25,32 @@ class ZipViewController: UIViewController, UICollectionViewDelegate, UICollectio
         //cell.configure(document: documents[indexPath.row])
         return cell
     }
-    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+      // 1
+      switch kind {
+      // 2
+      case UICollectionView.elementKindSectionHeader:
+        // 3  \(ZipSectionHeaderView.self)  sectionHeader
+        guard
+          let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: "sectionHeader",
+            for: indexPath) as? ZipSectionHeaderView
+          else {
+            fatalError("Invalid view type")
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //let searchTerm = searches[indexPath.section].searchTerm
+        headerView.label.text = "Zbiór Zip"//searchTerm
+        return headerView
+      default:
+        // 4
+        assert(false, "Invalid element type")
+      }
     }
 }
+//-------
+
 
 //    extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 //        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
